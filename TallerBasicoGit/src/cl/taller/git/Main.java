aa3347704476ed093055f9dde273ec50e06f75f8
grupo10ubb <gtobar@alumnos.ubiobio.cl>
2014-04-07 15:18:13 -0500

    Segundo cambio web

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mi segundo commit");
-		System.out.println("Linea agregada desde la web");
+		System.out.println("Modificado por segunda vez en web");
+		
 
 	}
 
